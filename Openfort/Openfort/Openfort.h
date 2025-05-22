bb6ef03c9b5ed940aff6d998dcca83c5cfaf6ac8
Pavel Gurkovskii <pavelgurkovskiy@gmail.com>
2025-05-22 10:40:41 +0200

    Initial commit

---
+++ Openfort/Openfort/Openfort.h
@@ -0,0 +1,18 @@
+//
+//  Openfort.h
+//  Openfort
+//
+//  Created by Pavel Gurkovskii on 13.06.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Openfort.
+FOUNDATION_EXPORT double OpenfortVersionNumber;
+
+//! Project version string for Openfort.
+FOUNDATION_EXPORT const unsigned char OpenfortVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Openfort/PublicHeader.h>
+
+
---
+++ Openfort/OpenfortTests/OpenfortTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenfortTests.swift
+//  OpenfortTests
+//
+//  Created by Pavel Gurkovskii on 13.06.2025.
+//
+
+import Testing
+@testable import Openfort
+
+struct OpenfortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
