18de5695e6abb3a252aa7d0fba5125806b716fdd
Pavlo Hurkovskiy <pavelgurkovskiy@gmail.com>
2025-07-04 14:40:54 +0200

    Added signUpGuest method

--- Sources/OpenfortSwift/Protocols/OFSignUpable.swift
+++ Sources/OpenfortSwift/Protocols/OFSignUpable.swift
@@ -5,3 +5,19 @@
 //  Created by Pavel Gurkovskii on 2025-07-01.
 //
 
+public protocol OFSignUpable: OFWebViewAccessable {
+
+}
+
+public extension OFSignUpable {
+    func signUpGuest(completion: @escaping (Result<Any?, Error>) -> Void) {
+        let js = "window.signUpGuestSync();"
+        webView?.evaluateJavaScript(js) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(result))
+            }
+        }
+    }
+}
--- Sources/OpenfortSwift/Resources/www/platform-bridge/openfort-sync.js
+++ Sources/OpenfortSwift/Resources/www/platform-bridge/openfort-sync.js
@@ -5,12 +5,20 @@
 //  Created by Pavel Gurkovskii on 2025-06-30.
 //
 
-window.logInWithEmailPasswordSync = function({ email, password, ecosystemGame }) {
-    window.openfort.logInWithEmailPassword(email, password, ecosystemGame)
+function handleResult(method, promise) {
+    promise
         .then(result => {
-            window.webkit.messageHandlers.userHandler.postMessage({ method: 'logInWithEmailPassword', success: true, data: result });
+            window.webkit.messageHandlers.userHandler.postMessage({ method: method, success: true, data: result });
         })
         .catch(error => {
-            window.webkit.messageHandlers.userHandler.postMessage({ method: 'logInWithEmailPassword', success: false, error: error && error.message ? error.message : String(error) });
+            window.webkit.messageHandlers.userHandler.postMessage({ method: method, success: false, error: error && error.message ? error.message : String(error) });
         });
+}
+
+window.logInWithEmailPasswordSync = function({ email, password, ecosystemGame }) {
+    handleResult('logInWithEmailPassword', window.openfort.logInWithEmailPassword(email, password, ecosystemGame));
+};
+
+window.signUpGuestSync = function() {
+    handleResult('signUpGuest', window.openfort.signUpGuest());
 };
