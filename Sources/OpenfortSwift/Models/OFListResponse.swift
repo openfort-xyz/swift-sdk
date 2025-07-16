283abc8cd828f33d97e82389261bff57b8dfd924
Pavlo Hurkovskiy <pavelgurkovskiy@gmail.com>
2025-07-16 16:26:53 +0200

    fixed OFListResponse

--- Sources/OpenfortSwift/Models/OFListResponse.swift
+++ Sources/OpenfortSwift/Models/OFListResponse.swift
@@ -5,14 +5,28 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
-public protocol OFListResponseProtocol {
-    var items: [String]? { get }
-    var error: String? { get }
-    var success: Bool? { get }
+public protocol EmbeddedAccountProtocol {
+    var owner: EmbeddedAccount.Owner { get }
+    var chainType: String { get }
+    var address: String { get }
+    var ownerAddress: String? { get }
+    var createdAt: Int? { get }
+    var implementationType: String? { get }
+    var chainId: String? { get }
 }
 
-public struct OFListResponse: Decodable, OFListResponseProtocol {
-    public let items: [String]?
-    public let error: String?
-    public let success: Bool?
+
+public struct EmbeddedAccount: Decodable, EmbeddedAccountProtocol {
+    public struct Owner: Decodable {
+        public let id: String
+    }
+    public let owner: Owner
+    public let chainType: String
+    public let address: String
+    public let ownerAddress: String?
+    public let createdAt: Int?
+    public let implementationType: String?
+    public let chainId: String?
 }
+
+public typealias OFListResponse = [EmbeddedAccount]
