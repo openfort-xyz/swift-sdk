d82f67b814151ac2a7f37ff2b6ce363ba924a283
Pavlo Hurkovskyi <>
2025-07-23 11:48:55 +0200

    Fixed naming of OFConfigureEmbeddedWalletDTO

--- Sources/OpenfortSwift/DTO/OFConfigureEmbeddedWalletDTO.swift
+++ Sources/OpenfortSwift/DTO/OFConfigureEmbeddedWalletDTO.swift
@@ -5,20 +5,20 @@
 //  Created by Pavlo Hurkovskyi on 2025-07-22.
 //
 
-public struct ShieldAuthenticationDTO: Codable {
+public struct OFShieldAuthenticationDTO: Codable {
     let auth: String
     let token: String
     let authProvider: String
     let tokenType: String
 }
 
-public struct RecoveryParamsDTO: Codable {
+public struct OFRecoveryParamsDTO: Codable {
     let recoveryMethod: String
     let password: String
 }
 
-public struct ConfigureEmbeddedWalletDTO: Codable {
+public struct OFConfigureEmbeddedWalletDTO: Codable {
     let chainId: String
-    let shieldAuthentication: ShieldAuthenticationDTO
-    let recoveryParams: RecoveryParamsDTO
+    let shieldAuthentication: OFShieldAuthenticationDTO
+    let recoveryParams: OFRecoveryParamsDTO
 }
