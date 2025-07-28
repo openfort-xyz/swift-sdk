f07806204009eb4cc1c7c33ddfff4881495c1a86
Pavlo Hurkovskyi <>
2025-07-28 16:01:24 +0200

    removed unnecessary firebase libs

--- Sources/OpenfortSwift/signature.js
+++ Sources/OpenfortSwift/signature.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const firebaseApp = firebase.initializeApp({
-    apiKey: 'AIzaSyCCUPOkncFnbTjAdXAOr0rMJSlZ7pZ6eoE',
-    authDomain: 'non-custodial-sample.firebaseapp.com',
-    projectId: 'non-custodial-sample',
-    appId: '1:1053697688933:web:593d3e0db64b08af2b0d8c',
-  });
 
   const openfort = new Openfort({
     baseConfiguration: {
@@ -16,49 +10,4 @@ document.addEventListener('DOMContentLoaded', async () => {
     },
   });
     
-  const auth = firebaseApp.auth();
-
-  const signMessage = async (message) => {
-    const signature = await openfort.signMessage(message);
-    if (!signature) {
-      addMessage(`Error: There was an error signing the message.`);
-    }
-    addMessage(`Signature: ${signature}`);
-  };
-
-  const logout = async () => {
-    await openfort.logout();
-    await auth.signOut();
-  };
-
-  auth.onIdTokenChanged(async (user) => {
-    if (user) {
-      const idToken = await user.getIdToken();
-      const player = await openfort.authenticateWithThirdPartyProvider({
-        provider:'firebase',
-        token:idToken,
-        tokenType:'idToken'
-      });
-      const embeddedState = openfort.getEmbeddedState();
-      console.log(embeddedState);
-      if (embeddedState !== 4) {
-        window.location.href = 'index.html';
-      }
-
-      document
-        .getElementById('sign-message-button')
-        .addEventListener('click', async () => {
-          await signMessage('Hello Message');
-        });
-
-      document
-        .getElementById('logout-button')
-        .addEventListener('click', async () => {
-          await logout();
-        });
-    } else {
-      addMessage('Signed out');
-      window.location.href = 'index.html';
-    }
-  });
 });
