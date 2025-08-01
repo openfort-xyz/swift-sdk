22d840f0b57b05dc4f881ae1e43fe5590808101e
Pavlo Hurkovskyi <>
2025-08-01 15:39:51 +0200

    fixed OFConfigureResponse

--- Sources/OpenfortSwift/Models/OFConfigureResponse.swift
+++ Sources/OpenfortSwift/Models/OFConfigureResponse.swift
@@ -5,10 +5,28 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
+public protocol OFConfigureOwnerResponseProtocol {
+    var id: String? { get }
+}
+
 public protocol OFConfigureResponseProtocol {
-    var configured: Bool? { get }
+    var ownerAddress: String? { get }
+    var chainId: Int? { get }
+    var owner: OFOwner? { get }
+    var implementationType: String? { get }
+    var chainType: String? { get }
+    var address: String? { get }
 }
 
 public struct OFConfigureResponse: Decodable, OFConfigureResponseProtocol {
-    public let configured: Bool?
+    public let ownerAddress: String?
+    public let chainId: Int?
+    public let owner: OFOwner?
+    public let implementationType: String?
+    public let chainType: String?
+    public let address: String?
+}
+
+public struct OFOwner: Decodable, OFConfigureOwnerResponseProtocol {
+    public let id: String?
 }
