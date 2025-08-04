94f14fb3b84ffcf19afe06e9e3ea34350e153146
Pavlo Hurkovskyi <>
2025-08-04 21:59:07 +0200

    fixed OFGetEthereumProviderParams

--- Sources/OpenfortSwift/DTO/OFGetEthereumProviderParams.swift
+++ Sources/OpenfortSwift/DTO/OFGetEthereumProviderParams.swift
@@ -6,9 +6,32 @@
 //
 
 public struct OFGetEthereumProviderParams: Codable {
-    public let options: [String: AnyCodable]?
+    public struct ProviderInfo: Codable {
+        public let icon: String?
+        public let name: String?
+        public let rdns: String?
+        
+        public init(icon: String? = nil, name: String? = nil, rdns: String? = nil) {
+            self.icon = icon
+            self.name = name
+            self.rdns = rdns
+        }
+    }
 
-    public init(options: [String: AnyCodable]? = nil) {
-        self.options = options
+    public let policy: String?
+    public let chains: [Int: String]?        // Changed key type to Int for Record<number, string>
+    public let providerInfo: ProviderInfo?   // New field
+    public let announceProvider: Bool?       // New field
+
+    public init(
+        policy: String? = nil,
+        chains: [Int: String]? = nil,
+        providerInfo: ProviderInfo? = nil,
+        announceProvider: Bool? = nil
+    ) {
+        self.policy = policy
+        self.chains = chains
+        self.providerInfo = providerInfo
+        self.announceProvider = announceProvider
     }
 }
