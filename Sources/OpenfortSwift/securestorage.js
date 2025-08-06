41412f0e7a7dd1db31253346457d29e8b4d64b59
Pavlo Hurkovskyi <>
2025-08-06 21:50:29 +0200

    added debugging

--- Sources/OpenfortSwift/securestorage.js
+++ Sources/OpenfortSwift/securestorage.js
@@ -1,4 +1,5 @@
 window.ReactNativeWebView = window.ReactNativeWebView || {};
 window.ReactNativeWebView.injectedObjectJson = function() {
+    window.webkit.messageHandlers.userHandler.postMessage({ method: 'shouldUseAppBackedStorage' });
     return JSON.stringify({ shouldUseAppBackedStorage: true });
 };
