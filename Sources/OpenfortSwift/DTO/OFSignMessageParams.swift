699db4f933485e697e58eef5a663917c3444fceb
Pavlo Hurkovskyi <>
2025-08-08 18:48:33 +0200

    hotfix

--- Sources/OpenfortSwift/DTO/OFSignMessageParams.swift
+++ Sources/OpenfortSwift/DTO/OFSignMessageParams.swift
@@ -6,7 +6,7 @@
 //
 
 public struct OFSignMessageParams: Codable, Sendable {
-    public struct Options: Codable {
+    public struct Options: Codable, Sendable {
         public let hashMessage: Bool?
         public let arrayifyMessage: Bool?
 
