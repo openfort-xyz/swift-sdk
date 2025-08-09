e9aee2f9a1a7db28ccefd2da7124cd42fc379f61
Pavlo Hurkovskyi <>
2025-08-09 17:33:27 +0200

    fixed OFPoolOAuthResponse

--- Sources/OpenfortSwift/ResponseModels/OFPoolOAuthResponse.swift
+++ Sources/OpenfortSwift/ResponseModels/OFPoolOAuthResponse.swift
@@ -5,12 +5,4 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
-public protocol OFPoolOAuthResponseProtocol {
-    var poolId: String? { get }
-    var expiresAt: String? { get }
-}
-
-public struct OFPoolOAuthResponse: OFPoolOAuthResponseProtocol, OFCodableSendable {
-    public let poolId: String?
-    public let expiresAt: String?
-}
+public typealias OFPoolOAuthResponse = OFAuthorizationResponse
