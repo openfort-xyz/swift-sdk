4f635790451a04a896184b052c65f16af0b24247
Pavlo Hurkovskyi <>
2025-08-09 18:05:00 +0200

    fixed OFSendSignatureTransactionIntentRequestResponse

--- Sources/OpenfortSwift/ResponseModels/OFSendSignatureTransactionIntentRequestResponse.swift
+++ Sources/OpenfortSwift/ResponseModels/OFSendSignatureTransactionIntentRequestResponse.swift
@@ -5,14 +5,4 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
-public protocol OFSendSignatureTransactionIntentRequestResponseProtocol {
-    var id: String? { get }
-    var status: String? { get }
-    var transactionHash: String? { get }
-}
-
-public struct OFSendSignatureTransactionIntentRequestResponse:  OFSendSignatureTransactionIntentRequestResponseProtocol, OFCodableSendable {
-    public let id: String?
-    public let status: String?
-    public let transactionHash: String?
-}
+public typealias OFSendSignatureTransactionIntentRequestResponse = OFTransactionIntentResponse
