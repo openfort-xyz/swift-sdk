67fb6de33b7a78fe7c488df31c8d34fe8a9ea68b
Pavlo Hurkovskyi <>
2025-08-09 18:17:01 +0200

    fixed OFUnlinkWalletResponse

--- Sources/OpenfortSwift/ResponseModels/OFUnlinkWalletResponse.swift
+++ Sources/OpenfortSwift/ResponseModels/OFUnlinkWalletResponse.swift
@@ -5,12 +5,4 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
-public protocol OFUnlinkWalletResponseProtocol {
-    var walletAddress: String? { get }
-    var unlinked: Bool? { get }
-}
-
-public struct OFUnlinkWalletResponse: OFUnlinkWalletResponseProtocol, OFCodableSendable {
-    public let walletAddress: String?
-    public let unlinked: Bool?
-}
+public typealias OFUnlinkWalletResponse = OFAuthPlayerResponse
