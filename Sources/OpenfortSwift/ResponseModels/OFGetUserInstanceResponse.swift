b572aff9a9498a69fe52cddae0de9809f1c04420
Pavlo Hurkovskyi <>
2025-08-11 17:58:53 +0200

    fixed OFGetUserInstanceResponse

--- Sources/OpenfortSwift/ResponseModels/OFGetUserInstanceResponse.swift
+++ Sources/OpenfortSwift/ResponseModels/OFGetUserInstanceResponse.swift
@@ -5,16 +5,4 @@
 //  Created by Pavel Gurkovskii on 2025-07-10.
 //
 
-public protocol OFGetUserInstanceResponseProtocol {
-    var id: String? { get }
-    var createdAt: Int? { get }
-    var player: String? { get }
-    var linkedAccounts: [OFLinkedAccountResponse]? { get }
-}
-
-public struct OFGetUserInstanceResponse: OFGetUserInstanceResponseProtocol, OFCodableSendable {
-    public var id: String?
-    public let createdAt: Int?
-    public let player: String?
-    public let linkedAccounts: [OFLinkedAccountResponse]?
-}
+public typealias OFGetUserInstanceResponse = OFAuthPlayerResponse
