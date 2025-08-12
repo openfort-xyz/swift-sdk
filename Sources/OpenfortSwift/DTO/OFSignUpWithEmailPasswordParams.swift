2914c91041971a9bd6db54863ff2da63b13b839f
Pavlo Hurkovskyi <>
2025-08-12 18:02:20 +0200

    fixed OFSignUpWithEmailPasswordParams

--- Sources/OpenfortSwift/DTO/OFSignUpWithEmailPasswordParams.swift
+++ Sources/OpenfortSwift/DTO/OFSignUpWithEmailPasswordParams.swift
@@ -5,16 +5,24 @@
 //  Created by Pavlo Hurkovskyi on 2025-07-25.
 //
 
-public struct OFSignUpWithEmailPasswordParams: Codable, Sendable {
+public struct OFSignUpWithEmailPasswordOptionsParams: OFCodableSendable {
+    public let data: [String: String]?
+    
+    public init(data: [String: String]? = nil) {
+        self.data = data
+    }
+}
+
+public struct OFSignUpWithEmailPasswordParams: OFCodableSendable {
     public let email: String
     public let password: String
-    public let name: String?
+    public let options: OFSignUpWithEmailPasswordOptionsParams?
     public let ecosystemGame: String?
 
-    public init(email: String, password: String, name: String? = nil, ecosystemGame: String? = nil) {
+    public init(email: String, password: String, options: OFSignUpWithEmailPasswordOptionsParams? = nil, ecosystemGame: String? = nil) {
         self.email = email
         self.password = password
-        self.name = name
+        self.options = options
         self.ecosystemGame = ecosystemGame
     }
 }
