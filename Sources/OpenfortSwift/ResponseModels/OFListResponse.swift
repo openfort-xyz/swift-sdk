b71008ac90996c66861192f6b4b05ae423b4f8f5
Pavlo Hurkovskyi <>
2025-08-14 11:44:11 +0200

    fixed EmbeddedAccount

--- Sources/OpenfortSwift/ResponseModels/OFListResponse.swift
+++ Sources/OpenfortSwift/ResponseModels/OFListResponse.swift
@@ -6,24 +6,36 @@
 //
 
 public protocol EmbeddedAccountProtocol {
-    var owner: OFOwner { get }
+    var user: String { get }
+    var id: String { get }
     var chainType: String { get }
     var address: String { get }
-    var ownerAddress: String? { get }
     var createdAt: Int? { get }
     var implementationType: String? { get }
-    var chainId: String? { get }
+    var factoryAddress: String? { get }
+    var salt: String? { get }
+    var accountType: String { get }
+    var chainId: Int? { get }
+    var ownerAddress: String? { get } // Deprecated
+    var type: String? { get } // Deprecated
+    var owner: OFOwner { get }
 }
 
 
 public struct EmbeddedAccount: EmbeddedAccountProtocol, OFCodableSendable {
-    public let owner: OFOwner
+    public let user: String
+    public let id: String
     public let chainType: String
     public let address: String
-    public let ownerAddress: String?
     public let createdAt: Int?
     public let implementationType: String?
-    public let chainId: String?
+    public let factoryAddress: String?
+    public let salt: String?
+    public let accountType: String
+    public let chainId: Int?
+    public let ownerAddress: String? // Deprecated
+    public let type: String? // Deprecated
+    public let owner: OFOwner
 }
 
 public typealias OFListResponse = [EmbeddedAccount]
