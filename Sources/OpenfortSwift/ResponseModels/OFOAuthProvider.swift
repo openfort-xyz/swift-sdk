151203ffd08f1bdeb6dafcadfade8010537e9d30
Pavlo Hurkovskyi <>
2025-08-14 13:45:27 +0200

    Added OFOAuthProvider

---
+++ Sources/OpenfortSwift/ResponseModels/OFOAuthProvider.swift
@@ -0,0 +1,16 @@
+//
+//  OFOAuthProvider.swift
+//  OpenfortSwift
+//
+//  Created by Pavlo Hurkovskyi on 2025-08-14.
+//
+
+public enum OFOAuthProvider: String {
+    case google = "google"
+    case twitter = "twitter"
+    case apple = "apple"
+    case facebook = "facebook"
+    case discord = "discord"
+    case epicGames = "epic_games"
+    case line = "line"
+}
