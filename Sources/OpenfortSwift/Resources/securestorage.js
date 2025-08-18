bf350fa6ab73cf7d5697eee3b9a6a05e15c3b852
Pavlo Hurkovskyi <>
2025-08-18 22:53:11 +0200

    added logging

--- Sources/OpenfortSwift/Resources/securestorage.js
+++ Sources/OpenfortSwift/Resources/securestorage.js
@@ -2,6 +2,7 @@
 //
 //window.ReactNativeWebView = window.ReactNativeWebView || {};
 window.ReactNativeWebView.injectedObjectJson = function() {
+    window.webkit.messageHandlers.userHandler.postMessage({ method: "shouldUseAppBackedStorage" });
     return JSON.stringify({ shouldUseAppBackedStorage: true });
 };
 
