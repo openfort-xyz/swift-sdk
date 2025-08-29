8d82bbf9371213c5c558333e3fcead50245f9b2f
Pavlo Hurkovskyi <>
2025-08-29 17:07:56 +0200

    fixed OFStoreCredentialsParams

--- Sources/OpenfortSwift/DTO/OFStoreCredentialsParams.swift
+++ Sources/OpenfortSwift/DTO/OFStoreCredentialsParams.swift
@@ -6,15 +6,13 @@
 //
 
 public struct OFStoreCredentialsParams: Codable, Sendable {
-    public let provider: String
-    public let token: String
-    public let tokenType: String
-    public let ecosystemGame: String?
+    public let player: String?
+    public let accessToken: String
+    public let refreshToken: String
 
-    public init(provider: String, token: String, tokenType: String, ecosystemGame: String? = nil) {
-        self.provider = provider
-        self.token = token
-        self.tokenType = tokenType
-        self.ecosystemGame = ecosystemGame
+    public init(player: String? = nil, accessToken: String, refreshToken: String) {
+        self.player = player
+        self.accessToken = accessToken
+        self.refreshToken = refreshToken
     }
 }
